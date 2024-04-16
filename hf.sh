f178fe700803df98996bf5ed941bbc439e8e86d5
brinenick511-venus <brinenick511@gmail.com>
2024-04-16 18:15:08 +0800

    modified:   hf.sh

--- hf.sh
+++ hf.sh
@@ -12,6 +12,24 @@ cat << EOF
 EOF
 }
 
+prompt_continue() {
+    while true; do
+        read -p "Do you want to continue? [y/n]: " input
+        case $input in
+            [Yy]* ) break;;
+            [Nn]* ) echo "Exiting program."; exit;;
+            * ) echo "Invalid input. Please enter y or n.";;
+        esac
+    done
+}
+
+for arg in "$@"; do
+    if [[ "$arg" == "--help" ]]; then
+        show_help
+        exit 1
+    fi
+done
+
 NAME=${1}
 TYPE=${2:-"model"}
 OUTPUT_DIR=${3:-"/data/yanghq/${TYPE}s/${NAME}/"}
@@ -23,12 +41,14 @@ fi
 
 if [ "$TYPE" = "model" ]; then
     echo "huggingface-cli download --local-dir-use-symlinks False --resume-download $NAME --local-dir $OUTPUT_DIR"
+    prompt_continue
     huggingface-cli download --local-dir-use-symlinks False --resume-download $NAME --local-dir $OUTPUT_DIR --token "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"
 elif [ "$TYPE" = "dataset" ]; then
     echo "huggingface-cli download --local-dir-use-symlinks False --repo-type dataset --resume-download $NAME --local-dir $OUTPUT_DIR"
+    prompt_continue
     huggingface-cli download --local-dir-use-symlinks False --repo-type dataset --resume-download $NAME --local-dir $OUTPUT_DIR --token "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"
 else
-    echo "传递给脚本的参数个数：$#"
+    echo "下载类型错误：$#"
     show_help
     exit 1
 fi
